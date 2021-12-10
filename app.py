34ec148c5ea1d254d7e4db65051f720931594354
Stanislav <stas.sor@gmail.com>
2021-12-10 13:15:21 +0300

    Первый рабочий прототип

--- app.py
+++ app.py
@@ -1,22 +1,61 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, WebSocket
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 import uvicorn
+from pymongo import MongoClient
+client = MongoClient('mongodb://localhost:27017')
+
+
+db = client.personwork
+
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Задание</title>
+    </head>
+    <body>
+        <h1>Сообщения</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Отправить</button>
+        </form>
+        <ol id='messages'>
+        </ol>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
 
-templates = Jinja2Templates(directory="templates")
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
 
-
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
-    return templates.TemplateResponse("index.html", {"request": request, "id": id})
-
-
-
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"{data}")
+        bills_post = db.person.find_one({'name': data})
+        await websocket.send_text(f"{bills_post}")
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000, )
+    uvicorn.run(app)
